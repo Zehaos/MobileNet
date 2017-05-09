562fe7e3510dfd398661bf32420d94e4d27ca932
zehao <shizehao.scu@gmail.com>
2017-05-09 14:18:22 +0800

    Add insert placeholder & test forzen graph script

---
+++ test/insert_placeholder.py
@@ -0,0 +1,36 @@
+import tensorflow as tf
+from nets.mobilenet import mobilenet
+from tensorflow.python.training import saver as saver_lib
+from tensorflow.python import pywrap_tensorflow
+
+input_checkpoint = '/home/zehao/PycharmProjects/MobileNet/mobilenet-model/model.ckpt-439074'
+
+# Where to save the modified graph
+save_path = '/home/zehao/PycharmProjects/MobileNet/mobilenet-model/with_placeholder'
+
+with tf.Graph().as_default() as graph:
+  input_images = tf.placeholder(tf.float32, [None, 224, 224, 3], 'MobileNet/input_images')
+  logits, predictions = mobilenet(inputs=input_images, num_classes=1001, is_training=False)
+  saver = tf.train.Saver()
+  with tf.Session() as sess:
+    var_list = {}
+    reader = pywrap_tensorflow.NewCheckpointReader(input_checkpoint)
+    var_to_shape_map = reader.get_variable_to_shape_map()
+    for key in var_to_shape_map:
+      try:
+        tensor = sess.graph.get_tensor_by_name(key + ":0")
+      except KeyError:
+        # This tensor doesn't exist in the graph (for example it's
+        # 'global_step' or a similar housekeeping element) so skip it.
+        continue
+      var_list[key] = tensor
+    saver = saver_lib.Saver(var_list=var_list)
+
+    # Restore variables
+    saver.restore(sess, input_checkpoint)
+
+    # Save new checkpoint and the graph
+    saver.save(sess, save_path+'/with_placeholder')
+    tf.train.write_graph(graph, save_path, 'graph.pbtxt')
+
+
--- test/test_forzen_graph.py
+++ test/test_forzen_graph.py
@@ -2,32 +2,45 @@ import tensorflow as tf
 from scipy.misc import imread, imresize
 import numpy as np
 
+# Read image
 img = imread("/home/zehao/Desktop/dog.png")
-img = imresize(img, (224,224,3))
+img = imresize(img, (224, 224, 3))
 img = img.astype(np.float32)
 img = np.expand_dims(img, 0)
 
+# Preprocess
+img = img / 255.
+img = img - 0.5
+img = img * 2.
 
-#Define the filename of the frozen graph
-graph_filename = "./frozen_model.pb"
+# Define the filename of the frozen graph
+graph_filename = "../mobilenet-model/with_placeholder/frozen_graph.pb"
 
-#Create a graph def object to read the graph
+# Create labels dict from labels.txt
+labels_file = "/home/zehao/Dataset/imagenet-data/labels.txt"
+labels_dict = {}
+with open(labels_file, 'r') as f:
+  for kv in [d.strip().split(':') for d in f]:
+    labels_dict[int(kv[0])] = kv[1]
+
+# Create a graph def object to read the graph
 with tf.gfile.GFile(graph_filename, "rb") as f:
-    graph_def = tf.GraphDef()
-    graph_def.ParseFromString(f.read())
+  graph_def = tf.GraphDef()
+  graph_def.ParseFromString(f.read())
 
-#Construct the graph and import the graph from graphdef
+# Construct the graph and import the graph from graphdef
 with tf.Graph().as_default() as graph:
-    tf.import_graph_def(graph_def)
+  tf.import_graph_def(graph_def)
 
-    #We define the input and output node we will feed in
-    input_node = graph.get_tensor_by_name('FIFOQueueV2:0')
-    output_node = graph.get_tensor_by_name('import/InceptionResnetV2/Logits/Predictions:0')
+  # We define the input and output node we will feed in
+  input_node = graph.get_tensor_by_name('import/MobileNet/input_images:0')
+  output_node = graph.get_tensor_by_name('import/MobileNet/Predictions/Softmax:0')
 
-    with tf.Session() as sess:
-        predictions = sess.run(output_node, feed_dict = {input_node: img})
-        print predictions
-        label_predicted = np.argmax(predictions[0])
+  with tf.Session() as sess:
+    predictions = sess.run(output_node, feed_dict={input_node: img})[0]
+    top_5_predictions = predictions.argsort()[-5:][::-1]
+    top_5_probabilities = predictions[top_5_predictions]
+    prediction_names = [labels_dict[i] for i in top_5_predictions]
 
-    print 'Predicted Flower:', labels_dict[label_predicted]
-    print 'Prediction probability:', predictions[0][label_predicted]
+    for i in xrange(len(prediction_names)):
+      print 'Prediction: %s, Probability: %s \n' % (prediction_names[i], top_5_probabilities[i])
