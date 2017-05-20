abfbcef7a77b621d1fa2c0b9f47fad5166547848
zehaos <asd293107>
2017-05-20 20:42:13 +0800

    summary image with bboxes

--- test/test_tfrecord.py
+++ test/test_tfrecord.py
@@ -3,7 +3,8 @@ from __future__ import division
 from __future__ import print_function
 
 import tensorflow as tf
-import numpy as np
+from preprocessing import preprocessing_factory
+from configs.kitti_config import config
 import cv2
 
 from datasets import dataset_factory
@@ -12,22 +13,42 @@ from tensorflow.contrib import slim
 dataset = dataset_factory.get_dataset(
   'kitti', 'train', '/home/zehao/Dataset/KITII/tfrecord')
 
-with tf.device('/cpu:0'):
-  provider = slim.dataset_data_provider.DatasetDataProvider(
-    dataset,
-    num_readers=1,
-    common_queue_capacity=20 * 1,
-    common_queue_min=10 * 1)
-  [image, shape, bbox, label] = provider.get(['image', 'shape', 'object/bbox', 'object/label'])
+def conver_box(bboxes, img_h, img_w):
+  [ymin, xmin, ymax, xmax] = tf.unstack(bboxes, axis=1)
+  img_h = tf.cast(img_h, tf.float32)
+  img_w = tf.cast(img_w, tf.float32)
+  ymin = tf.truediv(ymin, img_h)
+  xmin = tf.truediv(xmin, img_w)
+  ymax = tf.truediv(ymax, img_h)
+  xmax = tf.truediv(xmax, img_w)
+  return tf.expand_dims(tf.stack([ymin,xmin,ymax,xmax], axis=1), axis=0)
 
-with tf.Session() as sess:
-  coord = tf.train.Coordinator()
-  threads = tf.train.start_queue_runners(coord=coord)
+with tf.Graph().as_default() as graph:
+  with tf.device('/cpu:0'):
+    provider = slim.dataset_data_provider.DatasetDataProvider(
+      dataset,
+      num_readers=1,
+      common_queue_capacity=20 * 1,
+      common_queue_min=10 * 1)
+    [image, shape, bbox, label] = provider.get(['image', 'shape', 'object/bbox', 'object/label'])
 
-  [image, shape, bbox, label] = sess.run([image, shape, bbox, label])
-  print(shape)
-  print(bbox)
-  print(label)
-  image = image[:, :, ::-1]
-  cv2.imshow('show', image)
-  cv2.waitKey(0)
+    image_preprocessing_fn = preprocessing_factory.get_preprocessing(
+          'mobilenetdet',
+          is_training=True)
+
+    image, gt_labels, gt_bboxes = image_preprocessing_fn(image,
+                                                         config.IMG_HEIGHT,
+                                                         config.IMG_WIDTH,
+                                                         labels=label,
+                                                         bboxes=conver_box(bbox, config.IMG_HEIGHT, config.IMG_WIDTH),
+                                                         )
+  with tf.Session() as sess:
+    coord = tf.train.Coordinator()
+    threads = tf.train.start_queue_runners(coord=coord)
+    # [image, bbox, label] = sess.run([image, gt_bboxes, gt_labels])
+
+    summary_writer = tf.summary.FileWriter("/home/zehao/PycharmProjects/MobileNet/summary", sess.graph)
+    merge = tf.summary.merge_all()
+    merge = sess.run(merge)
+    summary_writer.add_summary(merge)
+    summary_writer.close()
