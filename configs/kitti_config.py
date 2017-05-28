079d1d76129101f4e4092eece2ce74df1fc6a512
zehaos <asd293107>
2017-05-28 21:05:20 +0800

    fea H <-> W

--- configs/kitti_config.py
+++ configs/kitti_config.py
@@ -7,8 +7,8 @@ config.IMG_HEIGHT = 1242
 config.IMG_WIDTH = 375
 
 # TODO(shizehao): infer fea shape in run time
-config.FEA_HEIGHT = 39
-config.FEA_WIDTH = 12
+config.FEA_HEIGHT = 12
+config.FEA_WIDTH = 39
 
 config.EPSILON = 1e-16
 
@@ -19,29 +19,22 @@ config.LOSS_COEF_CLASS = 1.0
 
 config.EXP_THRESH = 1.0
 
-config.BGR_MEANS = np.array([[[103.939, 116.779, 123.68]]])
+config.RBG_MEANS = np.array([[[ 123.68, 116.779, 103.939]]])
 
-def set_anchor_shape(anchor_shape):
-  # anchor_shape[:][0] = anchor_shape[:][0] / config.IMG_HEIGHT
-  # anchor_shape[:][1] = anchor_shape[:][1] / config.IMG_WIDTH
-
-  # Not scale
-  anchor_shape[:][0] = anchor_shape[:][0]
-  anchor_shape[:][1] = anchor_shape[:][1]
-  return anchor_shape
-
-
-# config.ANCHOR_SHAPE = set_anchor_shape(np.array([[36., 37.], [366., 174.], [115., 59.],
-#                                                  [162., 87.], [38., 90.], [258., 173.],
-#                                                  [224., 108.], [78., 170.], [72., 43.]], dtype=np.float32))
 
 def set_anchors(H, W):
   B = 9
-  anchor_shapes = np.reshape(
-      [np.array(
+  shape = np.array(
           [[  36.,  37.], [ 366., 174.], [ 115.,  59.],
            [ 162.,  87.], [  38.,  90.], [ 258., 173.],
-           [ 224., 108.], [  78., 170.], [  72.,  43.]])] * H * W,
+           [ 224., 108.], [  78., 170.], [  72.,  43.]])
+
+  # # scale
+  # shape[:, 0] = shape[:, 0] / config.IMG_HEIGHT
+  # shape[:, 1] = shape[:, 1] / config.IMG_WIDTH
+
+  anchor_shapes = np.reshape(
+      [shape] * H * W,
       (H, W, B, 2)
   )
   center_x = np.reshape(
